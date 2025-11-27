f68b8fb5af0d18f27ae07b2e775fe8b9278e8f80
Mat Cox <51195492+TehMuffinMoo@users.noreply.github.com>
2025-11-27 22:08:36 +0000

    Refactor logging and DNS query handling in qsaf.py
    Refactor logging and error handling, improve code readability, and update DNS query handling.

--- Docker/qsaf.py
+++ Docker/qsaf.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 import logging
 import struct
@@ -19,13 +20,16 @@ import gzip
 import httpx
 import json
 import pandas as pd
+import pyarrow.parquet as pq
 
-#########################################################	
+#########################################################
+# Setup logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
 
 with open('/var/run/qsaf.pid', 'w', encoding='utf-8') as f:
     f.write(str(os.getpid()))
-    
-line_number=0
+
+line_number = 0
 starttime = timeit.default_timer()
 
 config = configparser.ConfigParser()
@@ -33,171 +37,146 @@ config.read('/home/qsaf/config.ini')
 dns_server = config['dns']['forwarder']
 dns_server_type = config['dns']['type']
 
-## Check valid Syslog Type has been set
+# Validate Syslog Type
 log_format = config['syslog']['type']
 with open("/home/qsaf/regex.json", encoding="utf8") as regexfile:
     data = json.load(regexfile)
     regexconfig = [x for x in data['Formats'] if x['Name'] == log_format]
     if len(regexconfig) < 1:
-        print('Error! Unable to find any regex defined with name:',log_format)
+        logging.error(f"Unable to find regex defined with name: {log_format}")
         raise SystemExit
     elif len(regexconfig) > 1:
-        print('Error! More than one regex configuration was returned with name:',log_format)
+        logging.error(f"More than one regex configuration returned for name: {log_format}")
         raise SystemExit
-        
+
 view = config['dns']['view']
 role = config['server']['role']
 print_frequency = int(config['server']['print_frequency'])
 debug = config['debug']['enabled']
-if config['dns']['ignored_domains'] == '':
-    ignored_domains = None
-else:
-    ignored_domains = config['dns']['ignored_domains'].split(',')
+ignored_domains = config['dns']['ignored_domains'].split(',') if config['dns']['ignored_domains'] else None
 
 log_file = '/var/log/syslog-ng/collector.log'
-queries = 0 # Number of queries processed
-errors = 0  # Number of errors encountered
-threads = 0 # Number of active threads
-ignored = 0 # Number of queries ignored through the use of 'ignored_domains'
-skipped = 0 # Number of log lines skipped due to non-conforming regex. (Usually just noise in the logs)
+queries = errors = threads = ignored = skipped = 0
 
 #########################################################
 
-def send_dns_query(qip,qname,qtype,dns_server,type):
-    global errors
+def send_dns_query(qip, qname, qtype, dns_server, type):
+    global errors, threads
     try:
         if ':' in qip:
             qip = socket.inet_pton(socket.AF_INET6, qip)
         else:
             qip = socket.inet_aton(qip)
 
-        options = [dns.edns.GenericOption(65523,qip),                        #qip
-        #dns.edns.GenericOption(65524,binascii.unhexlify('000000000000')),   #source mac
-        dns.edns.GenericOption(65526,view.encode())]            #dns_view
+        options = [
+            dns.edns.GenericOption(65523, qip),
+            dns.edns.GenericOption(65526, view.encode())
+        ]
 
-        message = dns.message.make_query(qname, qtype, use_edns=True, options = options)
-        if debug=='True':
-            print('Payload: \n',message)
-            print('########################\n')
+        message = dns.message.make_query(qname, qtype, use_edns=True, options=options)
+        if debug == 'True':
+            logging.debug(f"Payload:\n{message}\n########################\n")
+
         try:
             match type:
                 case 'Plain':
-                    #dns.query.udp(message, dns_server, timeout=0.00000005)
                     dns.query.udp(message, dns_server, timeout=0.00000005)
                 case 'DoH':
-                    #dns.query.https(message, dns_server, timeout=1)
-                    dns.query.https(message, 'https://'+dns_server+'/dns-query', timeout=1)
+                    dns.query.https(message, f'https://{dns_server}/dns-query', timeout=1)
                 case 'DoT':
-                    #dns.query.tls(message, dns_server, timeout=1)
                     dns.query.tls(message, dns_server, timeout=1)
                 case _:
-                    print('Invalid DNS Server Type')
+                    logging.error('Invalid DNS Server Type')
         except:
-            if type!='Plain':
-                errors+=1
+            if type != 'Plain':
+                errors += 1
     except:
-        errors+=1
-
-    global threads
-    threads-=1
+        errors += 1
+    threads -= 1
 
 def start_job(line):
-	global threads
-	global queries
-	global ignored
-	global skipped
-	qip = qname = qtype = None
-	if debug=='True':
-		print(line)
-	if regexconfig:
-		for regex in regexconfig[0]['Regexes']:
-			z = re.match(regex['Regex'], line)
-			if z:
-				if len(z.groups()) == regex['Capture-Groups']:
-					qip = z.groups()[regex['IP-Group']]
-					qname = z.groups()[regex['Query-Group']]
-					qtype = z.groups()[regex['Type-Group']]
-					break
-			
-	if not (qip == None and qname == None and qtype == None):
-		ignore = False
-        ## Skip ignored domains
-		if ignored_domains is not None:
-			for igdom in ignored_domains:
-				if ignore == False:
-					if igdom in qname:
-						ignore = True
-		if ignore == False:
-			queries +=1
-			send_dns_query(qip,qname,qtype,dns_server,dns_server_type)
-		else:
-			ignored+=1
-			threads-=1
-		if print_frequency != 0:
-			if queries % print_frequency == 0:
-				print("\r", end="")
-				print("Queries:",queries, " / ","QPS:",int(queries/(timeit.default_timer() - starttime))," (Processed:",line_number," Active Threads:",threads," Errors:",errors," Ignored:",ignored," Skipped:",skipped, end=")")
-				print("\n")
-	else:
-		threads-=1
-		skipped+=1
-    
-def start_threadpool(content):                                              
-    global threads
-    global line_number
-    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-        if content is not None:                         
-            for line in content:                    
-                if isinstance(line, (bytes, bytearray)):
-                    line = str(line, "utf-8").strip()
-                else:                           
-                    line = line.strip()                  
-                threads+=1
-                line_number+=1
-                executor.submit(start_job(line))
+    global threads, queries, ignored, skipped
+    qip = qname = qtype = None
+    if debug == 'True':
+        logging.debug(line)
+    if regexconfig:
+        for regex in regexconfig[0]['Regexes']:
+            z = re.match(regex['Regex'], line)
+            if z and len(z.groups()) == regex['Capture-Groups']:
+                qip = z.groups()[regex['IP-Group']]
+                qname = z.groups()[regex['Query-Group']]
+                qtype = z.groups()[regex['Type-Group']]
+                break
+
+    if qip and qname and qtype:
+        ignore = False
+        if ignored_domains:
+            for igdom in ignored_domains:
+                if igdom in qname:
+                    ignore = True
+                    break
+        if not ignore:
+            queries += 1
+            send_dns_query(qip, qname, qtype, dns_server, dns_server_type)
+        else:
+            ignored += 1
+            threads -= 1
+        if print_frequency and queries % print_frequency == 0:
+            logging.info(f"Queries: {queries} / QPS: {int(queries/(timeit.default_timer() - starttime))} "
+                         f"(Processed: {line_number} Active Threads: {threads} Errors: {errors} Ignored: {ignored} Skipped: {skipped})")
+    else:
+        threads -= 1
+        skipped += 1
+
+def start_threadpool(content, executor):
+    global threads, line_number
+    if content:
+        for line in content:
+            if isinstance(line, (bytes, bytearray)):
+                line = str(line, "utf-8").strip()
+            else:
+                line = line.strip()
+            threads += 1
+            line_number += 1
+            executor.submit(start_job, line)
 
 #########################################################
 
-print('Debug mode is: ',debug)
+logging.info(f"Debug mode is: {debug}")
 
-if role =='forwarder':
-    print('Forwarder Mode Enabled. Logs will be collected from /var/log/syslog-ng/logs\r')
+executor = concurrent.futures.ThreadPoolExecutor(max_workers=50)
+
+if role == 'forwarder':
+    logging.info('Forwarder Mode Enabled. Logs will be collected from /var/log/syslog-ng/logs')
     lst = sorted(os.listdir('/var/log/syslog-ng/'), reverse=True)
     for filename in lst:
-        filepath = '/var/log/syslog-ng/'+filename
+        filepath = f'/var/log/syslog-ng/{filename}'
         if filename.endswith('.gz'):
-            content=gzip.open(filepath)
-            print("\n")
-            print('Processing:',filename)
-            print("\n")
-            start_threadpool(content)
+            logging.info(f"Processing: {filename}")
+            content = gzip.open(filepath)
+            start_threadpool(content, executor)
         elif filename == 'collector.log' or filename.endswith('.txt'):
-            print("\n")
-            print('Processing:',filepath)
-            print("\n")
-            content=open(filepath, 'r')
-            start_threadpool(content)
+            logging.info(f"Processing: {filepath}")
+            content = open(filepath, 'r')
+            start_threadpool(content, executor)
         elif filename.endswith('.parquet'):
-            print("\n")
-            print('Processing Parquet file:', filepath)
-            print("\n")
-            df = pd.read_parquet(filepath)
+            logging.info(f"Processing Parquet file: {filepath}")
+            parquet_file = pq.ParquetFile(filepath)
+            for batch in parquet_file.iter_batches(columns=['qip', 'qname', 'qtype'], batch_size=10000):
+                df = batch.to_pandas()
+                for row in df.itertuples(index=False):
+                    threads += 1
+                    line_number += 1
+                    executor.submit(send_dns_query, row.qip, row.qname, row.qtype, dns_server, dns_server_type)
+    logging.info('Log forwarding complete.')
 
-            if {'qip', 'qname', 'qtype'}.issubset(df.columns):
-                with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-                    for _, row in df.iterrows():
-                        threads += 1
-                        line_number += 1
-                        executor.submit(send_dns_query, row['qip'], row['qname'], row['qtype'], dns_server, dns_server_type)
-            else:
-                print(f"Error: Missing columns in {filename}")
-            print('\nLog forwarding complete.')
-elif (role =='both'):
-    print('Both Collector & Forwarder Mode enabled.\r')
+elif role == 'both':
+    logging.info('Both Collector & Forwarder Mode enabled.')
     content = tailer.follow(open(log_file))
-    start_threadpool(content)
-elif (role =='collector'):
-    content = 'None'
-    print('Collector Mode Enabled. Logs will not be forwarded during this session\r')
+    start_threadpool(content, executor)
 
-print("\n")
+elif role == 'collector':
+    logging.info('Collector Mode Enabled. Logs will not be forwarded during this session')
+
+executor.shutdown(wait=True)
+logging.info("Processing complete.")
